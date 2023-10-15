b707fd32751240c8cdb2fe4830dbcf11df1075ae
Semih Gül <semih.gul099@gmail.com>
2023-10-15 21:34:09 +0300

    kayıt ol tamamlandı

--- app/src/main/java/com/example/coursegradetracking/SignInFragment.java
+++ app/src/main/java/com/example/coursegradetracking/SignInFragment.java
@@ -1,5 +1,6 @@
 package com.example.coursegradetracking;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,9 +13,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.coursegradetracking.databinding.FragmentSignInBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class SignInFragment extends Fragment {
+    private FragmentSignInBinding binding;
+    private FirebaseAuth auth;
 
 
 
@@ -31,13 +41,17 @@ public class SignInFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        auth=FirebaseAuth.getInstance();
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+        binding = FragmentSignInBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+
     }
 
     @Override
@@ -45,6 +59,38 @@ public class SignInFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         goToLogIn(view);
+        userSignIn(view);
+    }
+
+    private void userSignIn(View view) {
+        Button button=binding.kayitolKayitOlButton;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name=binding.signInUserNameText.getText().toString();
+                String email=binding.singInUserEmailText.getText().toString();
+                String password=binding.singInPasswordText.getText().toString();
+                if (name.equals("")||email.equals("")||password.equals("")){
+                    Toast.makeText(getContext(),"Tüm alanları doldurun.",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Intent intent= new Intent(getContext(), MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(),e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                }
+            }
+        });
     }
 
     private void goToLogIn(View view) {
@@ -57,8 +103,6 @@ public class SignInFragment extends Fragment {
                 Navigation.findNavController(v).navigate(directions);
             }
         });
-
-
 
     }
 }
